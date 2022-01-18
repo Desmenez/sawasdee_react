7b55cb3bb23f8f354420b95dfa1c6b9dc50ae5c4
Desmenez <gobpk03@gmail.com>
2022-01-18 14:50:18 +0700

    responsive

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Suspense, useRef, useState, useEffect } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import {
   ContactShadows,
   Environment,
@@ -43,6 +43,12 @@ function Model() {
 
   // Cursor แสดงสถานะสีปัจจุบัน
   const [hovered, setHovered] = useState(null);
+
+  // set scale model
+  // const [active, setActive] = useState(false);
+  const { viewport } = useThree();
+  console.log(viewport);
+
   useEffect(() => {
     const cursor = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0)"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><g filter="url(#filter0_d)"><path d="M29.5 47C39.165 47 47 39.165 47 29.5S39.165 12 29.5 12 12 19.835 12 29.5 19.835 47 29.5 47z" fill="${snap.items[hovered]}"/></g><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/><text fill="#000" style="white-space:pre" font-family="Inter var, sans-serif" font-size="10" letter-spacing="-.01em"><tspan x="35" y="63">${hovered}</tspan></text></g><defs><clipPath id="clip0"><path fill="#fff" d="M0 0h64v64H0z"/></clipPath><filter id="filter0_d" x="6" y="8" width="47" height="47" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/><feOffset dy="2"/><feGaussianBlur stdDeviation="3"/><feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"/><feBlend in2="BackgroundImageFix" result="effect1_dropShadow"/><feBlend in="SourceGraphic" in2="effect1_dropShadow" result="shape"/></filter></defs></svg>`;
     const auto = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/></svg>`;
@@ -71,7 +77,11 @@ function Model() {
         e.stopPropagation(), (state.current = e.object.material.name)
       )}
     >
-      <group rotation={[-Math.PI, 0, 0]} scale={2}>
+      {/* viewport.width < 6 ? 1 : 2 */}
+      <group
+        rotation={[-Math.PI, 0, 0]}
+        scale={viewport.width < 6 ? viewport.width / 3.8 : 2}
+      >
         <mesh
           geometry={nodes.Nbackbody.geometry}
           material={materials.Backbody}
@@ -184,15 +194,6 @@ export default function App() {
         />
         <Suspense fallback={null}>
           <Environment preset="city" />
-          {/* <ContactShadows
-            rotation-x={Math.PI / 2}
-            position={[0, -0.8, 0]}
-            opacity={0.25}
-            width={10}
-            height={10}
-            blur={1.5}
-            far={0.8}
-          /> */}
 
           <OrbitControls
             minPolarAngle={Math.PI / 2}
@@ -200,6 +201,7 @@ export default function App() {
             enableZoom={false}
             enablePan={false}
           />
+
           <Model />
         </Suspense>
       </Canvas>
